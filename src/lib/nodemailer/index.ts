fa990e449498a48869d132c7d1151fba27d9e463
Muhammad Arbaz <arbazdogar20@gmail.com>
2025-12-29 14:46:04 +0100

    [FEAT]: Get automatically daily-news-summary

--- src/lib/nodemailer/index.ts
+++ src/lib/nodemailer/index.ts
@@ -1,5 +1,8 @@
 import nodemailer from "nodemailer";
-import { WELCOME_EMAIL_TEMPLATE } from "./templates";
+import {
+  NEWS_SUMMARY_EMAIL_TEMPLATE,
+  WELCOME_EMAIL_TEMPLATE,
+} from "./templates";
 
 export const transporter = nodemailer.createTransport({
   service: "Gmail",
@@ -24,6 +27,31 @@ export const sendWelcomeEmail = async ({
     to: email,
     subject: "Welcome to Stock App - your stock market toolkit is ready",
     text: "Thanks for joining stock app",
+    html: htmlTemplate,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+export const sendNewsSummaryEmail = async ({
+  email,
+  date,
+  newsContent,
+}: {
+  email: string;
+  date: string;
+  newsContent: string;
+}) => {
+  const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE.replace(
+    "{{date}}",
+    date
+  ).replace("{{newsContent}}", newsContent);
+
+  const mailOptions = {
+    from: `"Stock App News" <arbazdogar20@gmail.com>`,
+    to: email,
+    subject: `📈 Market News Summary Today - ${date}`,
+    text: `Today's market news summary from Stock App`,
     html: htmlTemplate,
   };
 
